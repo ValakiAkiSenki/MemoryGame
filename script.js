f4c211cf880ba2b188e9327e8c11b6f515a0274d
Valaki <fekete.kocsis500@gmail.com>
2022-10-20 14:10:52 +0200

    shuffle

--- script.js
+++ script.js
@@ -1,67 +1,99 @@
 let container = document.querySelector(".grid-container");
-const C_count = 5;
+const C_height = 5;
+const C_width = 7;
+const C_cardHeight = 80;
+let cardValues = [];
 let cards = [];
 
+GenerateCardValues();
+ShuffleValues();
 main()
 
-function EventOnClick(e){
-    if (cards.length >= 2){
-        cards.forEach(element => {
-            element.classList.remove("ani")
-            element.classList.add("aniB");
-        });
-        cards = [];
-    }
-
-    let qwe = e.target;
-    if (qwe.parentElement.classList.contains("item")) {
-        if (qwe.parentElement.classList.contains("ani")) {
-            qwe.parentElement.classList.remove("ani")
-            qwe.parentElement.classList.add("aniB");
-        }
-        else {
-            qwe.parentElement.classList.remove("aniB")
-            qwe.parentElement.classList.add("ani");
-        }
-        cards.push(qwe.parentElement);
-    }
-    else {
-        if (qwe.classList.contains("ani")) {
-            qwe.classList.remove("ani")
-            qwe.classList.add("aniB");
-        }
-        else {
-            qwe.classList.remove("aniB")
-            qwe.classList.add("ani");
-        }
-        cards.push(qwe);
-    }
-}
-
 function main(){
-    container.style["grid-template-columns"] = "repeat(" + C_count + ", 80px)";
-    container.style["grid-template-rows"] = "repeat(" + C_count + ", 80px)";
-    for(let i = 0; i < C_count*C_count; i++){
+    container.style["grid-template-columns"] = "repeat(" + C_width + ", " + C_cardHeight + "px)";
+    container.style["grid-template-rows"] = "repeat(" + C_height + ", " + C_cardHeight + "px)";
+    for(let i = 0; i < C_height*C_width; i++){
         let asd = document.createElement("div");
         asd.classList.add("card");
         asd.classList.add("item");
         asd.id = "a"+i;
-
+        
         let front = document.createElement("div");
         let back = document.createElement("div");
-
+        
         front.classList.add("face");
         front.classList.add("front");
         back.classList.add("face");
         back.classList.add("back");
         
+        let b = document.createElement("p");
+        b.style["line-height"] = C_cardHeight + "px";
+        b.innerHTML = cardValues[i];
+        back.appendChild(b);
+        
         asd.appendChild(front);
         asd.appendChild(back);
-
+        
         container.appendChild(asd);
     }
-
+    
     document.querySelectorAll(".item").forEach(element => {
         element.addEventListener("click", EventOnClick);
     });
+}
+
+function EventOnClick(e){
+    let qwe = e.target;
+
+    //#region gets the real item div
+    if (qwe.tagName == "P") {
+        qwe = qwe.parentElement.parentElement;
+    }
+    else if (qwe.parentElement.classList.contains("item")) {
+        qwe = qwe.parentElement;
+    }
+    //#endregion
+
+    //#region exits the function if card already picked
+    if (cards.includes(qwe.id)) return;
+    //#endregion
+
+    //#region if it's the 3rd card removes everything from the caards list
+    if (cards.length >= 2){
+        cards.forEach(element => {
+            let asd = document.querySelector("#" + element);
+            asd.classList.remove("ani")
+            asd.classList.add("aniB");
+        });
+        cards = [];
+    }
+    //#endregion
+
+    //#region animate
+    if (qwe.classList.contains("ani")) {
+        qwe.classList.remove("ani")
+        qwe.classList.add("aniB");
+    }
+    else {
+        qwe.classList.remove("aniB")
+        qwe.classList.add("ani");
+    }
+    //#endregion
+
+    cards.push(qwe.id);
+}
+
+function GenerateCardValues() {
+    for (let i = 0; i < C_height*C_width; i++) {
+        cardValues.push(i - Math.ceil(i/2));
+    }
+}
+
+function ShuffleValues() {
+    for (let i = cardValues.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let tmp = cardValues[i];
+        cardValues[i] = cardValues[j];
+        cardValues[j] = tmp;
+    }
 }
